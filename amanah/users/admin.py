c4a07e5ba2bd5a092587f189788c8a218cb6c886
vkm <genkhal25@gmail.com>
2025-12-27 15:23:51 +0100

    feat: users auth, swagger docs, and env setup

---
+++ amanah/users/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from .models import User
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'role', 'is_active')
+    list_filter = ('role',)
